a6bfb057a3a3fb35045f183b8336005df1ca3006
shaoorjan <janshaoor1@gmail.com>
2020-11-07 10:27:21 -0500

    removed syntax errors from rules.fcl file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
 
+import net.sourceforge.jFuzzyLogic.FIS;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // Load from 'FCL' file
+        String fileName = "C:\\Users\\jan_s\\Study\\Semester 2\\Expert System\\Project\\IntelligentCart\\src\\com\\company\\rules.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) {
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        
+
 
 
     }
